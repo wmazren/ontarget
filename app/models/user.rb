1f6cac4d87926862c207b61bd11400deaeef5ddb
Azren <w.m.azren@gmail.com>
2012-07-11 19:15:39 +0800

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,11 +20,10 @@ class User < ActiveRecord::Base
   end
   
   # Include default devise modules. Others available are:
-  # :token_authenticatable,
+  # :token_authenticatable, :confirmable
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me,
